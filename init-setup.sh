9da7da7de3e657ef7f3f34dbecd331da760cde7e
Paul Pignet <precision1998@gmail.com>
2023-06-06 10:10:19 +0200

    Update init-setup.sh

--- init-setup.sh
+++ init-setup.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-bash -c "$(curl -fsSL https://raw.githubusercontent.com/ohmybash/oh-my-bash/master/tools/install.sh)" && exit
+wget https://raw.githubusercontent.com/ohmybash/oh-my-bash/master/tools/install.sh
+echo "exit" >> install.sh
+bash install.sh
 curl -fsSL "https://raw.githubusercontent.com/monoamin/priv-utils/main/tweaks.aliases.sh" > "~/.oh-my-bash/custom/aliases/tweaks.aliases.sh"
 sed -i 's/OSH_THEME="font"/OSH_THEME="demula"/g' ~/.bashrc
 sed -i '/aliases=(/!b;n;s/general/general\n  tweaks/' ~/.bashrc
